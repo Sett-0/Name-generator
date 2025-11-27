7b549e99b01280d2e289be8e66d2c2d3c0133db3
Sett-0 <ampilov2000@mail.ru>
2025-11-27 18:12:04 +0300

    Add argunents parsing and remove j from vowels

--- gen.py
+++ gen.py
@@ -1,11 +1,19 @@
 from random import choice
+import argparse as ap 
 
-word_len_min, word_len_max = 3, 7
+parser = ap.ArgumentParser()
+tuple_type = lambda s: tuple(map(int, filter(lambda ch: ch not in '(, )', s)))
+parser.add_argument('--word_len', type=tuple_type, default=(3,7), help='Word length (min, max). Ex. (4,8), (4,4). Default = (3,7)')
+parser.add_argument('--num_words', type=int, default=1, help='Number of words in a line. Default = 1')
+parser.add_argument('--num_tries', type=int, default=15, help='Number of lines to generate. Default = 15')
+args = parser.parse_args()
+
+word_len_min, word_len_max = args.word_len
 word_lens  = list(range(word_len_min, word_len_max+1))
-num_words, num_tries = 2, 25
+num_words, num_tries = args.num_words, args.num_tries 
 
 letters = {chr(i) for i in range(97, 97+25+1)}
-vows = {'a', 'e', 'i', 'j', 'o', 'u'}
+vows = {'a', 'e', 'i', 'o', 'u'}
 cons = letters - vows
 cons, vows = list(cons), list(vows)
 
